2e208a70936bdd2c12b29b4f38103b0ca909b091
NguyenThiUtQuyen <quyen.ntu.57cntt@ntu.edu.vn>
2022-04-01 00:01:48 +0700

    Update 2

---
+++ Q_57130380_Intent/app/src/main/java/cntt61/q_57130380/MainActivity.java
@@ -0,0 +1,15 @@
+package cntt61.q_57130380;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
